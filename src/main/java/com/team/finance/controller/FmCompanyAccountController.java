5b9d20cd227feea28343ff07bf39eeb0f5b68d1f
guojiaxin <525429658@qq.com>
2017-12-28 14:24:03 +0800

    方法内容删除

--- src/main/java/com/team/finance/controller/FmCompanyAccountController.java
+++ src/main/java/com/team/finance/controller/FmCompanyAccountController.java
@@ -1,18 +1,13 @@
 package com.team.finance.controller;
 
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
-import com.isoft91.project.common.core.void;
-import com.isoft91.project.common.core.voidGenerator;
-import com.isoft91.project.model.FmCompanyAccount;
-import com.isoft91.project.service.FmCompanyAccountService;
+import com.team.finance.entity.FmCompanyAccount;
+import com.team.finance.service.FmCompanyAccountService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
 * Created by CodeGenerator on 2017/12/27.
@@ -25,33 +20,26 @@ public class FmCompanyAccountController {
 
     @PostMapping("/add")
     public void add(FmCompanyAccount fmCompanyAccount) {
-        fmCompanyAccountService.save(fmCompanyAccount);
-        return voidGenerator.genSuccessvoid();
+
     }
 
     @PostMapping("/delete")
     public void delete(@RequestParam Integer id) {
-        fmCompanyAccountService.deleteById(id);
-        return voidGenerator.genSuccessvoid();
+
     }
 
     @PostMapping("/update")
     public void update(FmCompanyAccount fmCompanyAccount) {
-        fmCompanyAccountService.update(fmCompanyAccount);
-        return voidGenerator.genSuccessvoid();
+    
     }
 
     @PostMapping("/detail")
     public void detail(@RequestParam Integer id) {
-        FmCompanyAccount fmCompanyAccount = fmCompanyAccountService.findById(id);
-        return voidGenerator.genSuccessvoid(fmCompanyAccount);
+
     }
 
     @PostMapping("/list")
     public void list(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer size) {
-        PageHelper.startPage(page, size);
-        List<FmCompanyAccount> list = fmCompanyAccountService.findAll();
-        PageInfo pageInfo = new PageInfo(list);
-        return voidGenerator.genSuccessvoid(pageInfo);
+
     }
 }
